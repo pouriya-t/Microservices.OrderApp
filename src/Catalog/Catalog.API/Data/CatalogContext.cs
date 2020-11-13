ae1ebef71c7510b0bee17a2ab123bb4b5daa98e9
Pouriya <pouriya.un@gmail.com>
2020-11-13 14:12:22 +0330

    Add project files.

---
+++ src/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Settings;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(ICatalogDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            Products = database.GetCollection<Product>(settings.CollectionName);
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
